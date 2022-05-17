e30daf8dc3a7aa73d273ae6729469ea98012b728
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2022-05-16 23:19:41 -0300

    specification

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+import { SpecificationRepository } from '../modules/cars/repositories/SpecificationRepository';
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+
+const specificationsRoutes = Router();
+const specificationRepository = new SpecificationRepository();
+
+specificationsRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationsService = new CreateSpecificationService(specificationRepository);
+
+  createSpecificationsService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+specificationsRoutes.get('/', async (request, response) => {
+  const specifications = await specificationRepository.list();
+  response.json(specifications);
+});
+
+export { specificationsRoutes };
