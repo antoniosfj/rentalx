8d10a79c1727406dffd2ddb0e9962ad9f044b3ad
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2022-05-29 21:23:06 -0300

    docker, docker-compose, database and typeORM

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'database',
+  port: 5432,
+  username: 'docker',
+  password: 'ignite',
+  database: 'rentalx',
+});
+
+dataSource.initialize();
