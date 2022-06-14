2f14f1d579c04bab5070c9f73273cb05f8725a49
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2022-06-14 00:59:05 -0300

    user added to request, @types/express/request, config uplolad

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,30 @@
-class UpdateUserAvatarUseCase {
-  async execute({ user_id, avatarFile }) {
+import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string|null;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id) as User;
+
+    // if (!user) {
+    //   throw new AppError('User does not exists.', 404);
+    // }
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
   }
 }
 
