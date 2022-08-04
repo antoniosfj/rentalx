e045eea9c307ee06937a6e70bd252ff8325e7b2a
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2022-08-03 23:14:15 -0300

    rentals module nad diagram image

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,14 @@
+import { ICreateRentalDTO } from '../dtos/ICreateRentalDTO';
+import { Rental } from '../infra/typeorm/entities/Rental';
+
+interface IRentalsRepository {
+  findOpenRentalByCarId(car_id: string): Promise<Rental | undefined | null>;
+  findOpenRentalByUserId(user_id: string): Promise<Rental | undefined | null>;
+  create({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental>;
+}
+
+export { IRentalsRepository };
---
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -0,0 +1,36 @@
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+
+import { IRentalsRepository } from '../IRentalsRepository';
+
+class RentalsRepositoryInMemory implements IRentalsRepository {
+  private rentals: Rental[] = [];
+
+  async findOpenRentalByCarId(car_id: string): Promise<Rental | undefined | null> {
+    return this.rentals.find((rental) => rental.car_id === car_id && !rental.end_date);
+  }
+  async findOpenRentalByUserId(user_id: string): Promise<Rental | undefined | null> {
+    return this.rentals.find((rental) => rental.user_id === user_id && !rental.end_date);
+  }
+  async create({
+    user_id, car_id, expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+    const nowDate = new Date();
+
+    Object.assign(rental, {
+      user_id,
+      car_id,
+      expected_return_date,
+      start_date: nowDate,
+      created_at: nowDate,
+      updated_at: nowDate,
+    });
+
+    this.rentals.push(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepositoryInMemory };
