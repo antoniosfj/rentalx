6e044e06ab9b5988252060dcf7140c3bb77a7eff
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2022-08-09 22:53:00 -0300

    supertest added

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+
+import { app } from '@shared/infra/http/app';
+
+describe('Create Category Controller', () => {
+  it('should be able to create a new category', async () => {
+    const response = await request(app).post('/categories').send({
+      name: 'Category name',
+      description: 'Category description',
+    });
+    expect(response.status).toBe(201);
+  });
+});
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,6 +3,10 @@ import DataSourceConnection from '@shared/infra/typeorm/data-source';
 
 const dataSource = DataSourceConnection();
 
+if (process.env.NODE_ENV === 'test') {
+  dataSource.setOptions({ database: 'rentalx_test', host: 'localhost' });
+}
+
 if (process.argv.some((arg) => arg.includes('migration:'))) {
   dataSource.setOptions({ host: 'localhost' });
 } else {
