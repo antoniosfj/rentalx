63ae3ddf3c873d1db2803ef62a38e899823be7d8
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2022-10-23 23:01:10 -0300

    refresh token authenticateuserusecase

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '67b49353e94bc6f3fc45e953fd26c0af',
+  expires_in_token: '15m',
+  secret_refresh_token: 'bfd15f200b4fb134b55ca06ace692d5e',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   dateDiff(start_date: Date, end_date: Date, unit?: string): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
+  addDays(days: number): Date;
 }
 
 export { IDateProvider };
